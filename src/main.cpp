c9156955f5bfc6cb4d2e6e021101f218e40da8e7
Potato. <105754685+dragon731012@users.noreply.github.com>
2025-12-04 17:59:34 -0600

    e

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "lemlib/api.hpp"
+#include "pros/adi.hpp"
 
 pros::MotorGroup left_motors({13, 9, 11}); 
 pros::MotorGroup right_motors({14, 10, 12});
@@ -16,12 +17,9 @@ pros::Imu imu(19);
 
 pros::Rotation hr1(5);
 pros::Rotation vr2(6);
-// horizontal tracking wheel
 lemlib::TrackingWheel horizontal_tracking_wheel(&hr1, lemlib::Omniwheel::NEW_275, -5.75);
-// vertical tracking wheel
 lemlib::TrackingWheel vertical_tracking_wheel(&vr2, lemlib::Omniwheel::NEW_275, -2.5);
 
-// odometry settings
 lemlib::OdomSensors sensors(&vertical_tracking_wheel, 
                             nullptr, 
                             &horizontal_tracking_wheel, 
@@ -29,7 +27,6 @@ lemlib::OdomSensors sensors(&vertical_tracking_wheel,
                             &imu
 );
 
-// lateral PID controller
 lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               3, // derivative gain (kD)
@@ -41,7 +38,6 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
                                               20 // maximum acceleration (slew)
 );
 
-// angular PID controller
 lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               10, // derivative gain (kD)
@@ -53,93 +49,76 @@ lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0 // maximum acceleration (slew)
 );
 
-// create the chassis
-lemlib::Chassis chassis(drivetrain, // drivetrain settings
-                        lateral_controller, // lateral PID settings
-                        angular_controller, // angular PID settings
-                        sensors // odometry sensors
+lemlib::Chassis chassis(drivetrain, 
+                        lateral_controller,
+                        angular_controller, 
+                        sensors 
 );
+
+//motors
+pros::Motor intake1(15, pros::E_MOTOR_GEARSET_18, false); 
+pros::Motor intake2(16, pros::E_MOTOR_GEARSET_18, true); 
+pros::Motor intake3(17, pros::E_MOTOR_GEARSET_18, false); 
+
+//pnumatics
+pros::ADIDigitalOut tongue(1); 
+bool tongueExtended = false;
 
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
 
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
 	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
+void disabled() {
 
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
+}
 
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
+void useTongue() {
+    tongueExtended=!tongueExtended;
+    tongue.set_value(tongueExtended);
+}
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
+void competition_initialize() {
+
+}
+
+void autonomous() {
+
+}
+
+void store(int val) {
+    intake1.move_velocity(1*val);
+    intake2.move_velocity(100);
+    intake3.move_velocity(100);
+}
+
+void score(int val) {
+    intake1.move_velocity(1*val);
+    intake2.move_velocity(-1*val);
+    intake3.move_velocity(-1*val);
+}
+
 void opcontrol() {
     left_motors.set_brake_mode(MOTOR_BRAKE_BRAKE);
     right_motors.set_brake_mode(MOTOR_BRAKE_BRAKE);
 
 	while (true) {
+        //setting modes
+        int val = (controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)*100)/127;
+        if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            store(val);
+        } else if (master.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            score(val);
+        }
+
+        //using pnumatics
+        if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2)) {
+            useTongue();
+        }
+
+        //driving
         int LeftX = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
         int LeftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 
